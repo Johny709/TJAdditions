44d7fbcb88bb71a323091324151a14d55b55c6d9
Johny709 <janjoseph.merida@gmail.com>
2025-04-08 17:43:09 -0400

    Fix UXV Universal circuit having wrong texture

--- src/main/java/com/johny/tj/items/TJMetaItem1.java
+++ src/main/java/com/johny/tj/items/TJMetaItem1.java
@@ -25,7 +25,7 @@ public class TJMetaItem1 extends MaterialMetaItem {
         UNIVERSAL_CIRCUITS[3] = HV_UNIVERSAL_CIRCUIT = addItem(1009, GAValues.VN[3].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[3]);
         UNIVERSAL_CIRCUITS[4] = EV_UNIVERSAL_CIRCUIT = addItem(1010, GAValues.VN[4].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[4]);
         UNIVERSAL_CIRCUITS[5] = IV_UNIVERSAL_CIRCUIT = addItem(1011, GAValues.VN[5].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[5]);
-        UNIVERSAL_CIRCUITS[6] = LUV_UNIVERSAL_CIRCUIT = addItem(10012, GAValues.VN[6].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[6]);
+        UNIVERSAL_CIRCUITS[6] = LUV_UNIVERSAL_CIRCUIT = addItem(1012, GAValues.VN[6].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[6]);
         UNIVERSAL_CIRCUITS[7] = ZPM_UNIVERSAL_CIRCUIT = addItem(1013, GAValues.VN[7].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[7]);
         UNIVERSAL_CIRCUITS[8] = UV_UNIVERSAL_CIRCUIT = addItem(1014, GAValues.VN[8].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[8]);
         UNIVERSAL_CIRCUITS[9] = UHV_UNIVERSAL_CIRCUIT = addItem(1015, GAValues.VN[9].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[9]);
@@ -34,6 +34,8 @@ public class TJMetaItem1 extends MaterialMetaItem {
         UNIVERSAL_CIRCUITS[12] = UMV_UNIVERSAL_CIRCUIT = addItem(1018, GAValues.VN[12].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[12]);
         UNIVERSAL_CIRCUITS[13] = UXV_UNIVERSAL_CIRCUIT = addItem(1019, GAValues.VN[13].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[13]);
         UNIVERSAL_CIRCUITS[14] = MAX_UNIVERSAL_CIRCUIT = addItem(1020, GAValues.VN[14].toLowerCase() + "_universal_circuit").setUnificationData(OrePrefix.circuit, CIRCUIT_TIERS[14]);
+
+        ENDER_FLUID_COVER = addItem(1021, "ender_fluid_cover");
     }
 
     @Override
