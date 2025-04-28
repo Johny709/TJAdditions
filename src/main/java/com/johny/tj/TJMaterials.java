c1f073d4e3a73ed89a42709f0942eb5620dd0ac3
Johny709 <janjoseph.merida@gmail.com>
2025-04-28 10:41:32 -0400

    generate frame flag for periodicium casings

--- src/main/java/com/johny/tj/TJMaterials.java
+++ src/main/java/com/johny/tj/TJMaterials.java
@@ -5,12 +5,13 @@ import gregtech.api.unification.material.IMaterialHandler;
 import static gregicadditions.GAMaterials.Periodicium;
 import static gregtech.api.unification.material.type.DustMaterial.MatFlags.GENERATE_PLATE;
 import static gregtech.api.unification.material.type.IngotMaterial.MatFlags.GENERATE_BOLT_SCREW;
+import static gregtech.api.unification.material.type.SolidMaterial.MatFlags.GENERATE_FRAME;
 
 @IMaterialHandler.RegisterMaterialHandler
 public class TJMaterials implements IMaterialHandler {
 
     @Override
     public void onMaterialsInit() {
-        Periodicium.addFlag(GENERATE_PLATE, GENERATE_BOLT_SCREW);
+        Periodicium.addFlag(GENERATE_PLATE, GENERATE_BOLT_SCREW, GENERATE_FRAME);
     }
 }
