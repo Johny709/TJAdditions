c08413bfc26f384e7eb155d572fca33b3333a9d7
Johny709 <janjoseph.merida@gmail.com>
2025-05-06 10:50:37 -0400

    Extract Maintenance and UI from MetaTileEntityXLTurbine to TJRotorHolderMultiblockController

--- src/main/java/com/johny/tj/machines/multi/electric/MetaTileEntityXLTurbine.java
+++ src/main/java/com/johny/tj/machines/multi/electric/MetaTileEntityXLTurbine.java
@@ -1,26 +1,18 @@
 package com.johny.tj.machines.multi.electric;
 
 import com.johny.tj.builder.handlers.XLTurbineWorkableHandler;
-import com.johny.tj.builder.multicontrollers.MultiblockDisplaysUtility;
-import com.johny.tj.gui.TJGuiTextures;
-import com.johny.tj.gui.TJHorizontoalTabListRenderer;
-import com.johny.tj.gui.TJTabGroup;
+import com.johny.tj.builder.multicontrollers.TJRotorHolderMultiblockController;
 import com.johny.tj.gui.TJWidgetGroup;
-import gregicadditions.GAConfig;
 import gregicadditions.capabilities.GregicAdditionsCapabilities;
 import gregicadditions.item.GAMetaItems;
-import gregicadditions.machines.GATileEntities;
-import gregicadditions.machines.multi.IMaintenance;
-import gregicadditions.machines.multi.multiblockpart.MetaTileEntityMaintenanceHatch;
 import gregtech.api.GTValues;
 import gregtech.api.capability.impl.FluidTankList;
 import gregtech.api.capability.impl.FuelRecipeLogic;
 import gregtech.api.capability.impl.ItemHandlerList;
-import gregtech.api.gui.GuiTextures;
-import gregtech.api.gui.ModularUI;
 import gregtech.api.gui.Widget;
-import gregtech.api.gui.widgets.*;
-import gregtech.api.gui.widgets.tab.ItemTabInfo;
+import gregtech.api.gui.widgets.AbstractWidgetGroup;
+import gregtech.api.gui.widgets.AdvancedTextWidget;
+import gregtech.api.gui.widgets.WidgetGroup;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
@@ -30,20 +22,13 @@ import gregtech.api.multiblock.FactoryBlockPattern;
 import gregtech.api.multiblock.PatternMatchContext;
 import gregtech.api.render.ICubeRenderer;
 import gregtech.api.render.OrientedOverlayRenderer;
-import gregtech.api.util.Position;
-import gregtech.api.util.XSTR;
 import gregtech.api.util.function.BooleanConsumer;
 import gregtech.common.metatileentities.electric.multiblockpart.MetaTileEntityRotorHolder;
 import gregtech.common.metatileentities.multi.electric.generator.MetaTileEntityLargeTurbine;
-import gregtech.common.metatileentities.multi.electric.generator.RotorHolderMultiblockController;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.Tuple;
 import net.minecraft.util.text.*;
 import net.minecraft.util.text.event.HoverEvent;
 import net.minecraft.world.World;
@@ -54,19 +39,16 @@ import org.apache.commons.lang3.tuple.Triple;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-import static gregicadditions.capabilities.MultiblockDataCodes.STORE_TAPED;
 import static gregtech.api.gui.widgets.AdvancedTextWidget.withButton;
 import static net.minecraft.util.text.TextFormatting.*;
 
 
-public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController implements IMaintenance {
+public class MetaTileEntityXLTurbine extends TJRotorHolderMultiblockController {
 
     public final MetaTileEntityLargeTurbine.TurbineType turbineType;
     public IFluidHandler exportFluidHandler;
@@ -75,22 +57,7 @@ public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController imp
     private int pageIndex;
     private final int pageSize = 6;
     private XLTurbineWorkableHandler xlTurbineWorkableHandler;
-    protected boolean doStructureCheck;
     private BooleanConsumer fastModeConsumer;
-
-    /**
-     * This value stores whether each of the 5 maintenance problems have been fixed.
-     * A value of 0 means the problem is not fixed, else it is fixed
-     * Value positions correspond to the following from left to right: 0=Wrench, 1=Screwdriver, 2=Soft Hammer, 3=Hard Hammer, 4=Wire Cutter, 5=Crowbar
-     */
-    protected byte maintenance_problems;
-
-    public static final XSTR XSTR_RAND = new XSTR();
-    private int timeActive;
-    private static final int minimumMaintenanceTime = 5184000; // 72 real-life hours = 5184000 ticks
-
-    // Used for data preservation with Maintenance Hatch
-    private boolean storedTaped = false;
 
     public MetaTileEntityXLTurbine(ResourceLocation metaTileEntityId, MetaTileEntityLargeTurbine.TurbineType turbineType) {
         super(metaTileEntityId, turbineType.recipeMap, GTValues.V[4]);
@@ -115,18 +82,8 @@ public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController imp
     }
 
     @Override
-    protected boolean checkStructureComponents(List<IMultiblockPart> parts, Map<MultiblockAbility<Object>, List<Object>> abilities) {
-        boolean canForm = super.checkStructureComponents(parts, abilities);
-        if (!canForm)
-            return false;
-
-        int maintenanceCount = abilities.getOrDefault(GregicAdditionsCapabilities.MAINTENANCE_HATCH, Collections.emptyList()).size();
-
-        return maintenanceCount == 1;
-    }
-
-    @Override
     protected void addDisplayText(List<ITextComponent> textList) {
+        super.addDisplayText(textList);
         if (isStructureFormed()) {
             ITextComponent totalEnergy = new TextComponentTranslation("tj.multiblock.extreme_turbine.energy");
             totalEnergy.appendText(" ");
@@ -158,8 +115,6 @@ public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController imp
 
             isWorkingText.getStyle().setColor(!xlTurbineWorkableHandler.isWorkingEnabled() ? YELLOW : !xlTurbineWorkableHandler.isActive() ? WHITE : GREEN);
             textList.add(isWorkingText);
-        } else {
-            MultiblockDisplaysUtility.isInvalid(textList, isStructureFormed());
         }
     }
 
@@ -250,18 +205,6 @@ public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController imp
         super.formStructure(context);
         this.exportFluidHandler = new FluidTankList(true, getAbilities(MultiblockAbility.EXPORT_FLUIDS));
         this.importItemHandler = new ItemHandlerList(getAbilities(MultiblockAbility.IMPORT_ITEMS));
-        if (getAbilities(GregicAdditionsCapabilities.MAINTENANCE_HATCH).isEmpty())
-            return;
-        MetaTileEntityMaintenanceHatch maintenanceHatch = getAbilities(GregicAdditionsCapabilities.MAINTENANCE_HATCH).get(0);
-        if (maintenanceHatch.getType() == 2 || !GAConfig.GT5U.enableMaintenance) {
-            this.maintenance_problems = 0b111111;
-        } else {
-            readMaintenanceData(maintenanceHatch);
-            if (maintenanceHatch.getType() == 0 && storedTaped) {
-                maintenanceHatch.setTaped(true);
-                storeTaped(false);
-            }
-        }
     }
 
     @Override
@@ -352,7 +295,7 @@ public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController imp
                 new MultiblockAbility[]{MultiblockAbility.IMPORT_FLUIDS, MultiblockAbility.IMPORT_ITEMS, MultiblockAbility.OUTPUT_ENERGY, GregicAdditionsCapabilities.MAINTENANCE_HATCH};
     }
     @Override
-    public boolean canShare(){
+    public boolean canShare() {
         return false;
     }
 
@@ -377,76 +320,10 @@ public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController imp
     }
 
     @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound data) {
-        super.writeToNBT(data);
-        data.setByte("Maintenance", maintenance_problems);
-        data.setInteger("ActiveTimer", timeActive);
-        return data;
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound data) {
-        super.readFromNBT(data);
-        maintenance_problems = data.getByte("Maintenance");
-        timeActive = data.getInteger("ActiveTimer");
-    }
-
-    @Override
-    public void writeInitialSyncData(PacketBuffer buf) {
-        super.writeInitialSyncData(buf);
-        buf.writeByte(maintenance_problems);
-        buf.writeInt(timeActive);
-    }
-
-    @Override
-    public void receiveInitialSyncData(PacketBuffer buf) {
-        super.receiveInitialSyncData(buf);
-        maintenance_problems = buf.readByte();
-        timeActive = buf.readInt();
-    }
-
-    @Override
-    public void receiveCustomData(int dataId, PacketBuffer buf) {
-        super.receiveCustomData(dataId, buf);
-        if (dataId == STORE_TAPED) {
-            storedTaped = buf.readBoolean();
-        }
-    }
-
-    @Override
-    protected ModularUI.Builder createUITemplate(EntityPlayer entityPlayer) {
-        ModularUI.Builder builder = ModularUI.extendedBuilder();
-        builder.image(-10, 0, 195, 217, TJGuiTextures.NEW_MULTIBLOCK_DISPLAY);
-        builder.bindPlayerInventory(entityPlayer.inventory, GuiTextures.SLOT ,-3, 134);
-        builder.widget(new LabelWidget(0, 7, getMetaFullName(), 0xFFFFFF));
-
-        TJTabGroup tabGroup = new TJTabGroup(() -> new TJHorizontoalTabListRenderer(TJHorizontoalTabListRenderer.HorizontalStartCorner.LEFT, TJHorizontoalTabListRenderer.VerticalLocation.BOTTOM), new Position(-10, 1));
-        List<Triple<String, ItemStack, AbstractWidgetGroup>> tabList = new ArrayList<>();
-        addNewTabs(tabList::add);
-        tabList.forEach(tabs -> tabGroup.addTab(new ItemTabInfo(tabs.getLeft(), tabs.getMiddle()), tabs.getRight()));
-        builder.widget(tabGroup);
-        return builder;
-    }
-
-    private void addNewTabs(Consumer<Triple<String, ItemStack, AbstractWidgetGroup>> tabs) {
-        TJWidgetGroup widgetDisplayGroup = new TJWidgetGroup(), widgetMaintenanceGroup = new TJWidgetGroup(), rotorWidgetGroup = new TJWidgetGroup();
-        tabs.accept(new ImmutableTriple<>("tj.multiblock.tab.display", this.getStackForm(), mainDisplayTab(widgetDisplayGroup::addWidgets)));
-        tabs.accept(new ImmutableTriple<>("tj.multiblock.tab.maintenance", GATileEntities.MAINTENANCE_HATCH[0].getStackForm(), maintenanceTab(widgetMaintenanceGroup::addWidgets)));
+    protected void addNewTabs(Consumer<Triple<String, ItemStack, AbstractWidgetGroup>> tabs) {
+        super.addNewTabs(tabs);
+        TJWidgetGroup rotorWidgetGroup = new TJWidgetGroup();
         tabs.accept(new ImmutableTriple<>("tj.multiblock.tab.rotor", GAMetaItems.HUGE_TURBINE_ROTOR.getStackForm(), rotorTab(rotorWidgetGroup::addWidgets)));
-    }
-
-    private AbstractWidgetGroup mainDisplayTab(Function<Widget, WidgetGroup> widgetGroup) {
-        widgetGroup.apply(new AdvancedTextWidget(10, 18, this::addDisplayText, 0xFFFFFF)
-                .setMaxWidthLimit(180).setClickHandler(this::handleDisplayClick));
-        widgetGroup.apply(new ToggleButtonWidget(172, 169, 18, 18, TJGuiTextures.POWER_BUTTON, this::isWorkingEnabled, this::setWorkingEnabled)
-                .setTooltipText("machine.universal.toggle.run.mode"));
-        return widgetGroup.apply(new ToggleButtonWidget(172, 133, 18, 18, TJGuiTextures.CAUTION_BUTTON, this::getDoStructureCheck, this::setDoStructureCheck)
-                .setTooltipText("machine.universal.toggle.check.mode"));
-    }
-
-    private AbstractWidgetGroup maintenanceTab(Function<Widget, WidgetGroup> widgetGroup) {
-        return widgetGroup.apply(new AdvancedTextWidget(10, 18, this::addMaintenanceDisplayText, 0xFFFFFF)
-                .setMaxWidthLimit(180));
     }
 
     private AbstractWidgetGroup rotorTab(Function<Widget, WidgetGroup> widgetGroup) {
@@ -454,103 +331,14 @@ public class MetaTileEntityXLTurbine extends RotorHolderMultiblockController imp
                 .setMaxWidthLimit(180).setClickHandler(this::handleRotorDisplayClick));
     }
 
-    private void addMaintenanceDisplayText(List<ITextComponent> textList) {
-        MultiblockDisplaysUtility.maintenanceDisplay(textList, maintenance_problems, hasProblems());
-    }
-
+    @Override
     public boolean isWorkingEnabled() {
         return xlTurbineWorkableHandler.isWorkingEnabled();
     }
 
+    @Override
     public void setWorkingEnabled(boolean isWorking) {
         xlTurbineWorkableHandler.setWorkingEnabled(isWorking);
         this.markDirty();
     }
-
-    private boolean getDoStructureCheck() {
-        if (isStructureFormed())
-            this.doStructureCheck = false;
-        return this.doStructureCheck;
-    }
-
-    private void setDoStructureCheck(boolean check) {
-        if (isStructureFormed()) {
-            this.doStructureCheck = true;
-            invalidateStructure();
-            this.structurePattern = createStructurePattern();
-        }
-    }
-
-    /**
-     * Sets the maintenance problem corresponding to index to fixed
-     *
-     * @param index the index of the maintenance problem
-     */
-    public void setMaintenanceFixed(int index) {
-        this.maintenance_problems |= 1 << index;
-    }
-
-    /**
-     * Used to cause a single random maintenance problem
-     */
-    protected void causeProblems() {
-        this.maintenance_problems &= ~(1 << ((int) (XSTR_RAND.nextFloat()*5)));
-    }
-
-    /**
-     *
-     * @return the byte value representing the maintenance problems
-     */
-    public byte getProblems() {
-        return maintenance_problems;
-    }
-
-    /**
-     *
-     * @return the amount of maintenance problems the multiblock has
-     */
-    public int getNumProblems() {
-        return 6 - Integer.bitCount(maintenance_problems);
-    }
-
-    /**
-     *
-     * @return whether the multiblock has any maintenance problems
-     */
-    public boolean hasProblems() {
-        return this.maintenance_problems < 63;
-    }
-
-    /**
-     * Used to calculate whether a maintenance problem should happen based on machine time active
-     * @param duration duration in ticks to add to the counter of active time
-     */
-
-    public void calculateMaintenance(int duration) {
-        MetaTileEntityMaintenanceHatch maintenanceHatch = getAbilities(GregicAdditionsCapabilities.MAINTENANCE_HATCH).get(0);
-        if (maintenanceHatch.getType() == 2 || !GAConfig.GT5U.enableMaintenance) {
-            return;
-        }
-
-        timeActive += duration;
-        if (minimumMaintenanceTime - timeActive <= 0)
-            if(XSTR_RAND.nextFloat() - 0.75f >= 0) {
-                causeProblems();
-                maintenanceHatch.setTaped(false);
-            }
-    }
-
-    private void readMaintenanceData(MetaTileEntityMaintenanceHatch hatch) {
-        if (hatch.hasMaintenanceData()) {
-            Tuple<Byte, Integer> data = hatch.readMaintenanceData();
-            this.maintenance_problems = data.getFirst();
-            this.timeActive = data.getSecond();
-        }
-    }
-
-    public void storeTaped(boolean isTaped) {
-        this.storedTaped = isTaped;
-        writeCustomData(STORE_TAPED, buf -> buf.writeBoolean(isTaped));
-    }
-
 }
