d56645857543d8920d3d1101c4e3440929e4fdb6
Johny709 <janjoseph.merida@gmail.com>
2025-07-15 07:52:39 -0400

    Fix Architect Workbench overclock

--- src/main/java/tj/builder/handlers/ArchitectWorkbenchWorkableHandler.java
+++ src/main/java/tj/builder/handlers/ArchitectWorkbenchWorkableHandler.java
@@ -1,80 +1,32 @@
 package tj.builder.handlers;
 
-import gregicadditions.GAUtility;
-import gregicadditions.GAValues;
-import gregtech.api.capability.GregtechTileCapabilities;
 import gregtech.api.capability.IEnergyContainer;
-import gregtech.api.capability.IWorkable;
-import gregtech.api.metatileentity.MTETrait;
 import gregtech.api.metatileentity.MetaTileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.PacketBuffer;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemHandlerHelper;
-import tj.capability.IItemFluidHandlerInfo;
-import tj.capability.TJCapabilities;
+import tj.capability.impl.AbstractWorkableHandler;
+import tj.util.ItemStackHelper;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.function.*;
+import java.util.function.IntFunction;
+import java.util.function.IntSupplier;
+import java.util.function.LongSupplier;
+import java.util.function.Supplier;
 
-public class ArchitectWorkbenchWorkableHandler extends MTETrait implements IWorkable, IItemFluidHandlerInfo {
+public class ArchitectWorkbenchWorkableHandler extends AbstractWorkableHandler {
 
-    private final Supplier<IItemHandlerModifiable> itemInputs;
-    private final Supplier<IItemHandlerModifiable> itemOutputs;
-    private final Supplier<IEnergyContainer> energyInputs;
-    private final IntFunction<IItemHandlerModifiable> inputBus;
-    private final LongSupplier maxVoltage;
-    private final IntSupplier parallel;
     private ItemStack catalyst;
     private ItemStack input;
     private ItemStack output;
-    private boolean isWorking = true;
-    private boolean isActive;
-    private boolean wasActiveAndNeedsUpdate;
-    private boolean isDistinct;
-    private int energyPerTick;
-    private int progress;
-    private int maxProgress;
-    private int lastInputIndex;
-    private int busCount;
 
     public ArchitectWorkbenchWorkableHandler(MetaTileEntity metaTileEntity, Supplier<IItemHandlerModifiable> itemInputs, Supplier<IItemHandlerModifiable> itemOutputs, Supplier<IEnergyContainer> energyInputs, IntFunction<IItemHandlerModifiable> inputBus, LongSupplier maxVoltage, IntSupplier parallel) {
-        super(metaTileEntity);
-        this.itemInputs = itemInputs;
-        this.itemOutputs = itemOutputs;
-        this.energyInputs = energyInputs;
-        this.inputBus = inputBus;
-        this.maxVoltage = maxVoltage;
-        this.parallel = parallel;
-    }
-
-    public void initialize(int busCount) {
-        this.lastInputIndex = 0;
-        this.busCount = busCount;
-        int tier = GAUtility.getTierByVoltage(this.maxVoltage.getAsLong());
-        this.energyPerTick = GAValues.VA[tier];
+        super(metaTileEntity, itemInputs, itemOutputs, energyInputs, inputBus, maxVoltage, parallel);
     }
 
     @Override
-    public void update() {
-        if (this.wasActiveAndNeedsUpdate)
-            this.setActive(false);
-
-        if (!this.isWorking || this.progress < 1 && !this.startRecipe()) {
-            this.wasActiveAndNeedsUpdate = true;
-            return;
-        }
-
-        if (this.progress < this.maxProgress) {
-            this.progressRecipe();
-        } else this.completeRecipe();
-    }
-
-    private boolean startRecipe() {
+    protected boolean startRecipe() {
         boolean canStart = false;
         IItemHandlerModifiable itemInputs = this.isDistinct ? this.inputBus.apply(this.lastInputIndex) : this.itemInputs.get();
         if (this.findCatalyst(itemInputs) && this.findInputs(itemInputs)) {
@@ -83,46 +35,36 @@ public class ArchitectWorkbenchWorkableHandler extends MTETrait implements IWork
             compound.setString("BaseName", Item.REGISTRY.getNameForObject(this.input.getItem()).toString());
             compound.setInteger("BaseData", this.input.getMetadata());
             this.output.setTagCompound(compound);
-            int recipeTickDuration = Math.round((float) 200L / GAUtility.getTierByVoltage(this.maxVoltage.getAsLong()));
-            this.maxProgress = Math.max(1, recipeTickDuration);
+            this.maxProgress = this.calculateOverclock(30, 200, 2.8F);
             this.wasActiveAndNeedsUpdate = false;
-            this.setActive(true);
             canStart = true;
+            if (!this.isActive)
+                this.setActive(true);
         }
         if (++this.lastInputIndex == this.busCount)
             this.lastInputIndex = 0;
         return canStart;
     }
 
-    private void progressRecipe() {
-        if (this.energyInputs.get().getEnergyStored() >= this.energyPerTick) {
-            this.energyInputs.get().removeEnergy(this.energyPerTick);
-            this.progress++;
-        } else if (this.progress > 0)
+    @Override
+    protected boolean completeRecipe() {
+        if (ItemHandlerHelper.insertItemStacked(this.itemOutputs.get(), this.output, true).isEmpty()) {
+            ItemHandlerHelper.insertItemStacked(this.itemOutputs.get(), this.output, false);
+            this.catalyst = null;
+            this.input = null;
+            this.output = null;
+            return true;
+        } else if (this.progress > 1)
             this.progress--;
-    }
-
-    private void completeRecipe() {
-        if (!ItemHandlerHelper.insertItemStacked(this.itemOutputs.get(), this.output, true).isEmpty()) {
-            this.progress--;
-            return;
-        }
-        ItemHandlerHelper.insertItemStacked(this.itemOutputs.get(), this.output, false);
-        this.progress = 0;
-        this.catalyst = null;
-        this.input = null;
-        this.output = null;
+        return false;
     }
 
     private boolean findCatalyst(IItemHandlerModifiable itemInputs) {
         for (int i = 0; i < itemInputs.getSlots(); i++) {
             ItemStack stack = itemInputs.getStackInSlot(i);
-            NBTTagCompound tagCompound = stack.getTagCompound();
-            if (tagCompound != null) {
-                if (tagCompound.hasKey("Shape") && tagCompound.hasKey("BaseName") && tagCompound.hasKey("BaseData")) {
-                    this.catalyst = stack;
-                    return true;
-                }
+            if (this.isArchitectureStack(stack.getTagCompound())) {
+                this.catalyst = stack;
+                return true;
             }
         }
         return false;
@@ -133,12 +75,10 @@ public class ArchitectWorkbenchWorkableHandler extends MTETrait implements IWork
         int count = 0;
         for (int i = 0; i < itemInputs.getSlots() && availableParallels != 0; i++) {
             ItemStack stack = itemInputs.getStackInSlot(i);
-            NBTTagCompound tagCompound = stack.getTagCompound();
-            if (stack.isEmpty() || tagCompound != null && tagCompound.hasKey("Shape") && tagCompound.hasKey("BaseName") && tagCompound.hasKey("BaseData"))
+            if (stack.isEmpty() || this.isArchitectureStack(stack.getTagCompound()))
                 continue;
-            if (this.input == null) {
+            if (this.input == null)
                 this.input = stack.copy();
-            }
             if (!stack.isItemEqual(this.input))
                 continue;
             int reminder = Math.min(stack.getCount(), availableParallels);
@@ -150,34 +90,13 @@ public class ArchitectWorkbenchWorkableHandler extends MTETrait implements IWork
         return count > 0;
     }
 
-    @Override
-    public void receiveCustomData(int id, PacketBuffer buffer) {
-        super.receiveCustomData(id, buffer);
-        if (id == 1) {
-            this.isActive = buffer.readBoolean();
-            this.metaTileEntity.scheduleRenderUpdate();
-        }
-    }
-
-    @Override
-    public void writeInitialData(PacketBuffer buffer) {
-        buffer.writeBoolean(this.isActive);
-    }
-
-    @Override
-    public void receiveInitialData(PacketBuffer buffer) {
-        this.isActive = buffer.readBoolean();
+    private boolean isArchitectureStack(NBTTagCompound tagCompound) {
+        return tagCompound != null && tagCompound.hasKey("Shape") && tagCompound.hasKey("BaseName") && tagCompound.hasKey("BaseData");
     }
 
     @Override
     public NBTTagCompound serializeNBT() {
-        NBTTagCompound compound = new NBTTagCompound();
-        compound.setInteger("progress", this.progress);
-        compound.setInteger("maxProgress", this.maxProgress);
-        compound.setBoolean("isWorking", this.isWorking);
-        compound.setBoolean("isDistinct", this.isDistinct);
-        compound.setBoolean("isActive", this.isActive);
-        compound.setBoolean("wasActiveAndNeedsUpdate", this.wasActiveAndNeedsUpdate);
+        NBTTagCompound compound = super.serializeNBT();
         if (this.catalyst != null)
             compound.setTag("catalyst", this.catalyst.serializeNBT());
         if (this.input != null)
@@ -189,94 +108,12 @@ public class ArchitectWorkbenchWorkableHandler extends MTETrait implements IWork
 
     @Override
     public void deserializeNBT(NBTTagCompound compound) {
-        this.maxProgress = compound.getInteger("maxProgress");
-        this.progress = compound.getInteger("progress");
-        this.isWorking = compound.getBoolean("isWorking");
-        this.isDistinct = compound.getBoolean("isDistinct");
-        this.isActive = compound.getBoolean("isActive");
-        this.wasActiveAndNeedsUpdate = compound.getBoolean("wasActiveAndNeedsUpdate");
+        super.deserializeNBT(compound);
         if (compound.hasKey("catalyst"))
             this.catalyst = new ItemStack(compound.getCompoundTag("catalyst"));
         if (compound.hasKey("input"))
             this.input = new ItemStack(compound.getCompoundTag("input"));
         if (compound.hasKey("output"))
             this.output = new ItemStack(compound.getCompoundTag("output"));
-    }
-
-    @Override
-    public String getName() {
-        return "RecipeWorkable";
-    }
-
-    @Override
-    public int getNetworkID() {
-        return TraitNetworkIds.TRAIT_ID_WORKABLE;
-    }
-
-    @Override
-    public <T> T getCapability(Capability<T> capability) {
-        if (capability == TJCapabilities.CAPABILITY_ITEM_FLUID_HANDLING)
-            return TJCapabilities.CAPABILITY_ITEM_FLUID_HANDLING.cast(this);
-        return capability == GregtechTileCapabilities.CAPABILITY_WORKABLE ? GregtechTileCapabilities.CAPABILITY_WORKABLE.cast(this) : null;
-    }
-
-    public void setDistinct(boolean distinct) {
-        this.isDistinct = distinct;
-    }
-
-    public boolean isDistinct() {
-        return this.isDistinct;
-    }
-
-    @Override
-    public int getProgress() {
-        return this.progress;
-    }
-
-    @Override
-    public int getMaxProgress() {
-        return this.maxProgress;
-    }
-
-    public double getProgressPercent() {
-        return this.getMaxProgress() == 0 ? 0.0 : this.getProgress() / (this.getMaxProgress() * 1.0);
-    }
-
-    public boolean hasNotEnoughEnergy() {
-        return this.isActive && this.energyInputs.get().getEnergyStored() < this.energyPerTick;
-    }
-
-    @Override
-    public boolean isActive() {
-        return this.isActive;
-    }
-
-    @Override
-    public List<ItemStack> getItemInputs() {
-        return this.input != null ? Collections.singletonList(this.input) : Collections.emptyList();
-    }
-
-    @Override
-    public List<ItemStack> getItemOutputs() {
-        return this.output != null ? Collections.singletonList(this.output) : Collections.emptyList();
-    }
-
-    @Override
-    public boolean isWorkingEnabled() {
-        return this.isWorking;
-    }
-
-    @Override
-    public void setWorkingEnabled(boolean isWorking) {
-        this.isWorking = isWorking;
-        this.metaTileEntity.markDirty();
-    }
-
-    public void setActive(boolean isActive) {
-        this.isActive = isActive;
-        if (!this.metaTileEntity.getWorld().isRemote) {
-            this.writeCustomData(1, buffer -> buffer.writeBoolean(isActive));
-            this.metaTileEntity.markDirty();
-        }
     }
 }
---
+++ src/main/java/tj/capability/impl/AbstractWorkableHandler.java
@@ -0,0 +1,209 @@
+package tj.capability.impl;
+
+import gregtech.api.capability.GregtechTileCapabilities;
+import gregtech.api.capability.IEnergyContainer;
+import gregtech.api.capability.IWorkable;
+import gregtech.api.metatileentity.MTETrait;
+import gregtech.api.metatileentity.MetaTileEntity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+import java.util.function.*;
+
+public abstract class AbstractWorkableHandler extends MTETrait implements IWorkable {
+
+    protected final Supplier<IItemHandlerModifiable> itemInputs;
+    protected final Supplier<IItemHandlerModifiable> itemOutputs;
+    protected final Supplier<IEnergyContainer> energyInputs;
+    protected final IntFunction<IItemHandlerModifiable> inputBus;
+    protected final LongSupplier maxVoltage;
+    protected final IntSupplier parallel;
+    protected boolean isWorking = true;
+    protected boolean isActive;
+    protected boolean wasActiveAndNeedsUpdate;
+    protected boolean isDistinct;
+    protected boolean hasProblem;
+    protected long energyPerTick;
+    protected int progress;
+    protected int maxProgress;
+    protected int lastInputIndex;
+    protected int busCount;
+
+    public AbstractWorkableHandler(MetaTileEntity metaTileEntity, Supplier<IItemHandlerModifiable> itemInputs, Supplier<IItemHandlerModifiable> itemOutputs, Supplier<IEnergyContainer> energyInputs, IntFunction<IItemHandlerModifiable> inputBus, LongSupplier maxVoltage, IntSupplier parallel) {
+        super(metaTileEntity);
+        this.itemInputs = itemInputs;
+        this.itemOutputs = itemOutputs;
+        this.energyInputs = energyInputs;
+        this.inputBus = inputBus;
+        this.maxVoltage = maxVoltage;
+        this.parallel = parallel;
+    }
+
+    public void initialize(int busCount) {
+        this.lastInputIndex = 0;
+        this.busCount = busCount;
+    }
+
+    @Override
+    public void update() {
+        if (this.wasActiveAndNeedsUpdate && this.isActive)
+            this.setActive(false);
+
+        if (!this.isWorking || this.progress < 1 && !this.startRecipe()) {
+            this.wasActiveAndNeedsUpdate = true;
+            return;
+        } else this.progressRecipe();
+
+        if (this.progress >= this.maxProgress) {
+            if (this.completeRecipe()) {
+                this.progress = 0;
+                this.hasProblem = false;
+            } else this.hasProblem = true;
+        }
+    }
+
+    /**
+     * @return true if the recipe can start
+     */
+    protected boolean startRecipe() {
+        return false;
+    }
+
+    protected void progressRecipe() {
+        if (this.energyInputs.get().getEnergyStored() >= this.energyPerTick) {
+            this.energyInputs.get().removeEnergy(this.energyPerTick);
+            this.progress++;
+        } else if (this.progress > 1)
+            this.progress--;
+    }
+
+    /**
+     * @return true if the recipe can be completed
+     */
+    protected boolean completeRecipe() {
+        return false;
+    }
+
+    protected int calculateOverclock(long baseEnergy, int duration, float multiplier) {
+        long voltage = this.maxVoltage.getAsLong();
+        baseEnergy *= 4;
+        for (int i = 1; duration > 1 && baseEnergy <= voltage; i++) {
+            duration /= multiplier;
+            baseEnergy *= 4;
+        }
+        this.energyPerTick = baseEnergy;
+        return Math.max(1, duration);
+    }
+
+    @Override
+    public void receiveCustomData(int id, PacketBuffer buffer) {
+        super.receiveCustomData(id, buffer);
+        if (id == 1) {
+            this.isActive = buffer.readBoolean();
+            this.metaTileEntity.scheduleRenderUpdate();
+        }
+    }
+
+    @Override
+    public void writeInitialData(PacketBuffer buffer) {
+        buffer.writeBoolean(this.isActive);
+    }
+
+    @Override
+    public void receiveInitialData(PacketBuffer buffer) {
+        this.isActive = buffer.readBoolean();
+    }
+
+    @Override
+    public NBTTagCompound serializeNBT() {
+        NBTTagCompound compound = new NBTTagCompound();
+        compound.setInteger("progress", this.progress);
+        compound.setInteger("maxProgress", this.maxProgress);
+        compound.setLong("energyPerTick", this.energyPerTick);
+        compound.setBoolean("isWorking", this.isWorking);
+        compound.setBoolean("isDistinct", this.isDistinct);
+        compound.setBoolean("isActive", this.isActive);
+        compound.setBoolean("wasActiveAndNeedsUpdate", this.wasActiveAndNeedsUpdate);
+        compound.setBoolean("hasProblem", this.hasProblem);
+        return compound;
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound compound) {
+        this.maxProgress = compound.getInteger("maxProgress");
+        this.progress = compound.getInteger("progress");
+        this.energyPerTick = compound.getLong("energyPerTick");
+        this.isWorking = compound.getBoolean("isWorking");
+        this.isDistinct = compound.getBoolean("isDistinct");
+        this.isActive = compound.getBoolean("isActive");
+        this.wasActiveAndNeedsUpdate = compound.getBoolean("wasActiveAndNeedsUpdate");
+        this.hasProblem = compound.getBoolean("hasProblem");
+    }
+
+    @Override
+    public String getName() {
+        return "RecipeWorkable";
+    }
+
+    @Override
+    public int getNetworkID() {
+        return TraitNetworkIds.TRAIT_ID_WORKABLE;
+    }
+
+    @Override
+    public <T> T getCapability(Capability<T> capability) {
+        return capability == GregtechTileCapabilities.CAPABILITY_WORKABLE ? GregtechTileCapabilities.CAPABILITY_WORKABLE.cast(this) : null;
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.isDistinct = distinct;
+    }
+
+    public boolean isDistinct() {
+        return this.isDistinct;
+    }
+
+    @Override
+    public int getProgress() {
+        return this.progress;
+    }
+
+    @Override
+    public int getMaxProgress() {
+        return this.maxProgress;
+    }
+
+    public double getProgressPercent() {
+        return this.getMaxProgress() == 0 ? 0.0 : this.getProgress() / (this.getMaxProgress() * 1.0);
+    }
+
+    public boolean hasNotEnoughEnergy() {
+        return this.isActive && this.energyInputs.get().getEnergyStored() < this.energyPerTick;
+    }
+
+    @Override
+    public boolean isActive() {
+        return this.isActive;
+    }
+
+    @Override
+    public boolean isWorkingEnabled() {
+        return this.isWorking;
+    }
+
+    @Override
+    public void setWorkingEnabled(boolean isWorking) {
+        this.isWorking = isWorking;
+        this.metaTileEntity.markDirty();
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+        if (!this.metaTileEntity.getWorld().isRemote) {
+            this.writeCustomData(1, buffer -> buffer.writeBoolean(isActive));
+            this.metaTileEntity.markDirty();
+        }
+    }
+}
