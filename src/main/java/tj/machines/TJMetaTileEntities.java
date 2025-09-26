ae72116b86514e4dec51f2dfd1af0c4e630f504a
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-09-26 16:17:32 -0400

    add Compressed Chest

--- src/main/java/tj/machines/TJMetaTileEntities.java
+++ src/main/java/tj/machines/TJMetaTileEntities.java
@@ -97,6 +97,8 @@ public class TJMetaTileEntities {
     public static MetaTileEntityLargeChiselWorkbench LARGE_CHISEL_WORKBENCH;
     public static MetaTileEntityLargeEnchanter LARGE_ENCHANTER;
     public static MetaTileEntityMegaFusion MEGA_FUSION;
+    public static MetaTileEntityCompressedChest COMPRESSED_CHEST;
+    public static MetaTileEntityCompressedChest COMPRESSED_CRATE;
     public static GAMetaTileEntityEnergyHatch[] ENERGY_INPUT_HATCH_256A = new GAMetaTileEntityEnergyHatch[14];
     public static GAMetaTileEntityEnergyHatch[] ENERGY_OUTPUT_HATCH_256A = new GAMetaTileEntityEnergyHatch[14];
     public static MetaTileEntityLargeAtmosphereCollector[] LARGE_ATMOSPHERE_COLLECTOR = new MetaTileEntityLargeAtmosphereCollector[3];
@@ -217,7 +219,7 @@ public class TJMetaTileEntities {
             CHISEL_WORKBENCH[i] = GregTechAPI.registerMetaTileEntity(chiselID++, new MetaTileEntityChiselWorkbench(TJId("chisel_workbench_" + GAValues.VN[tier]), tier));
         for (int i = 0, tier = 1; i < ENCHANTER.length; i++, tier++) // occupies ID range 5337 - 5350
             ENCHANTER[i] = GregTechAPI.registerMetaTileEntity(enchanterID++, new MetaTileEntityEnchanter(TJId("enchanter_" + GAValues.VN[tier]), tier));
-
+        COMPRESSED_CHEST = GregTechAPI.registerMetaTileEntity(5406, new MetaTileEntityCompressedChest(TJId("compressed_chest")));
 
         // range 5500+ -> misc
         ACCELERATOR_ANCHOR_POINT = GregTechAPI.registerMetaTileEntity(5500, new MetaTileEntityAcceleratorAnchorPoint(TJId("accelerator_anchor_point")));
