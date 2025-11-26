f5468fb7913a9e5622b9f0ac8f1a65a6041f9579
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-11-25 20:10:52 -0500

    Energy Ports add on to EU capacity of fusion reactors

--- src/main/java/tj/mixin/gregicality/TileEntityFusionReactorMixin.java
+++ src/main/java/tj/mixin/gregicality/TileEntityFusionReactorMixin.java
@@ -1,17 +1,29 @@
 package tj.mixin.gregicality;
 
+import gregicadditions.GAValues;
 import gregicadditions.machines.multi.TileEntityFusionReactor;
+import gregtech.api.capability.IEnergyContainer;
+import gregtech.api.capability.impl.EnergyContainerHandler;
+import gregtech.api.capability.impl.EnergyContainerList;
+import gregtech.api.capability.impl.FluidTankList;
+import gregtech.api.capability.impl.ItemHandlerList;
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.multiblock.BlockWorldState;
 import gregtech.api.multiblock.FactoryBlockPattern;
+import gregtech.api.multiblock.PatternMatchContext;
 import gregtech.api.recipes.RecipeMap;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import tj.blocks.AdvEnergyPortCasings;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import tj.blocks.EnergyPortCasings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +31,9 @@ import java.util.function.Predicate;
 
 @Mixin(value = TileEntityFusionReactor.class, remap = false)
 public abstract class TileEntityFusionReactorMixin extends RecipeMapMultiblockController implements ITileEntityFusionReactorMixin {
+
+    @Shadow
+    private EnergyContainerList inputEnergyContainers;
 
     public TileEntityFusionReactorMixin(ResourceLocation metaTileEntityId, RecipeMap<?> recipeMap) {
         super(metaTileEntityId, recipeMap);
@@ -30,14 +45,35 @@ public abstract class TileEntityFusionReactorMixin extends RecipeMapMultiblockCo
         return pattern;
     }
 
+    @Inject(method = "formStructure", at = @At(value = "INVOKE", target = "Lgregicadditions/machines/multi/TileEntityFusionReactor;initializeAbilities()V"),
+            locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+    private void injectFormStructure(PatternMatchContext context, CallbackInfo ci, long energyStored) {
+        int energyPorts = context.getOrDefault("EnergyPort", new ArrayList<>()).size();
+        inputInventory = new ItemHandlerList(getAbilities(MultiblockAbility.IMPORT_ITEMS));
+        inputFluidInventory = new FluidTankList(true, getAbilities(MultiblockAbility.IMPORT_FLUIDS));
+        outputInventory = new ItemHandlerList(getAbilities(MultiblockAbility.EXPORT_ITEMS));
+        outputFluidInventory = new FluidTankList(true, getAbilities(MultiblockAbility.EXPORT_FLUIDS));
+        List<IEnergyContainer> energyInputs = getAbilities(MultiblockAbility.INPUT_ENERGY);
+        inputEnergyContainers = new EnergyContainerList(energyInputs);
+        long euCapacity = (energyInputs.size() + energyPorts) * 10000000L * (long) Math.pow(2, getTier() - 6);
+        energyContainer = new EnergyContainerHandler(this, euCapacity, GAValues.V[getTier()], 0, 0, 0) {
+            @Override
+            public String getName() {
+                return "EnergyContainerInternal";
+            }
+        };
+        ((EnergyContainerHandler) energyContainer).setEnergyStored(energyStored);
+        ci.cancel();
+    }
+
     @Unique
     public Predicate<BlockWorldState> energyPortPredicate(int tier) {
         return (blockWorldState) -> {
             IBlockState blockState = blockWorldState.getBlockState();
-            if (blockState.getBlock() instanceof AdvEnergyPortCasings) {
-                AdvEnergyPortCasings abilityCasings = (AdvEnergyPortCasings) blockState.getBlock();
-                AdvEnergyPortCasings.AbilityType tieredCasingType = abilityCasings.getState(blockState);
-                List<AdvEnergyPortCasings.AbilityType> currentCasing = blockWorldState.getMatchContext().getOrCreate("EnergyPort", ArrayList::new);
+            if (blockState.getBlock() instanceof EnergyPortCasings) {
+                EnergyPortCasings abilityCasings = (EnergyPortCasings) blockState.getBlock();
+                EnergyPortCasings.AbilityType tieredCasingType = abilityCasings.getState(blockState);
+                List<EnergyPortCasings.AbilityType> currentCasing = blockWorldState.getMatchContext().getOrCreate("EnergyPort", ArrayList::new);
                 currentCasing.add(tieredCasingType);
                 return currentCasing.get(0).getName().equals(tieredCasingType.getName()) && currentCasing.get(0).getTier() >= tier;
             }
